0c2e3ec3ea98539a7fd23e96454e794e88e4b58a
Rahul Mishra <rahulmishra4317@gmail.com>
2024-10-15 19:28:06 +0530

    checking

--- Chapter 4 – Lists and Tuples/List_practice/list_comprehensions.py
+++ Chapter 4 – Lists and Tuples/List_practice/list_comprehensions.py
@@ -5,7 +5,7 @@ print(sqaures)
 # 2. Create a list of even numbers from 1 to 20 using a list comprehension
 even_numbers = [x for x in range(1,21) if x % 2 ==0]
 print(even_numbers)
-
+#hello
 # 3. Create a list of the first letter of each word in a given list of words
 words = ["apple", "banana", "cherry", "date"]
 first_letter = [word[0] for word in words]
