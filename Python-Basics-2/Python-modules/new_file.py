a70571c08615b109134b5dbe8a09bc8f3af13b4d
Rahul <rahulmishra4317@gmail.com>
2024-10-22 23:30:41 +0530

    from PC

---
+++ Python-Basics-2/Python-modules/new_file.py
@@ -0,0 +1,15 @@
+# import printNumbers as PN
+
+# from printNumbers import printBackwards as PB , printForward as PF
+# PN.printBackwards(5)
+# PN.printForward(5)
+
+
+# PF(6)
+# PB(6)
+
+from  printNumbers import *
+
+printForward(5)
+print("\n")
+printBackwards(10)
