1360582c2d47ceda33121c01154110234286a9b1
Rahul Mishra <rahulmishra4317@gmail.com>
2024-10-27 00:09:22 +0530

    Dunders - double underscore in python : Learning

--- 3-OOPs/Dunder/dunder_intro.py
+++ 3-OOPs/Dunder/dunder_intro.py
@@ -7,3 +7,11 @@ res = number.__add__(number2)
 print(res)
 
 
+number3 = 200
+number4 = 500
+
+result = number3.__add__(number4)
+result2 = number3+number4
+print(f"The result from result - {result} and result2 is {result2}")
+
+print("These are dunders -> which are double underscore")
