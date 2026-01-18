fe3b91ac12f69214e77676df431e67ff3b4e84ab
Rahul Mishra <rahulmishra4317@gmail.com>
2026-01-18 23:03:22 +0530

    Learning python -- List challenges

---
+++ 99-Practice/03-Lists/Lists Challenge/02_split_the_bill.py
@@ -0,0 +1,10 @@
+"""
+Instructions
+Suppose you and three of your friends all went out for a meal together. The bill comes due, and all of you decide to split the bill evenly.
+Here is a Python list of prices for what was ordered:
+bill = [13.99, 28.75, 9.99, 9.99, 6.95, 7.45, 16.45, 16.45]
+Using this bill list, calculate the total amount and then divide it four ways.
+Initialize the total with zero and loop through the bill list to add everything up.
+With the total, store what each person has to pay in a my_share variable and then print the result to the terminal.
+
+"""
